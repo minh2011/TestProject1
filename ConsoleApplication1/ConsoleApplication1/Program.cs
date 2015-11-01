73dc053c4d7a8b2c9415b4d51929f2a99f4bb219
Minh Nguyen <minh_nguyen_2001@hotmail.com>
2015-11-01 14:40:36 -0800

    checking in project

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class TestA
+    {
+        public int X { get; set; } = 1;
+
+        public int Y { get; set; } = 1;
+    }
+}
